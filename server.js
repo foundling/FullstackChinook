2b5db4b94ba26a4a4cb29eee6ae718d89888a9fb
Alex Ramsdell <alexramsdell@gmail.com>
2017-08-29 23:15:30 -0700

    add comment about nginx for static stuff. about to switch to koa.

--- server.js
+++ server.js
@@ -7,13 +7,19 @@ const client = require(path.join(__dirname,'db','client'));
 const api = require(path.join(__dirname, 'api'));
 const { utf8JSON, logger } = require(path.join(__dirname,'lib'));
 const PORT = process.env.PORT || 3000; 
+const publicDir = path.join(__dirname,'public','build');
+const docsDir = path.join(__dirname,'public','build','docs');
 
-app.use(express.static(path.join(__dirname,'public','build')));
-app.use('/api/docs',express.static(path.join(__dirname,'public','build','docs'))); 
+const startMSg = () => console.log('up and running on Port ', PORT)
 
+// middleware
 app.use(cors());
 app.use(utf8JSON);
 app.use(logger);
+
+// routes
+app.use(express.static(publicDir)); // use nginx instead
+app.use('/api/docs',express.static(docsDir)); // use nginx instead 
 app.use('/api', api);
 
 module.exports = {
