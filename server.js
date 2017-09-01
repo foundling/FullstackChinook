61199799d1deaa9156cad93a98a71d2e39b4aaa0
Alex Ramsdell <alexramsdell@gmail.com>
2017-09-01 13:26:30 -0700

    fix middleware issue, wasn't yielding next in /routes middleware.

--- server.js
+++ server.js
@@ -1,42 +1,34 @@
 const cors = require('kcors');
 const path = require('path');
+const mount = require('koa-mount');
 const Koa = require('koa');
 const KoaRouter = require('koa-router');
-const serve = require('koa-static-server');
+const serve = require('koa-static');
 const PORT = process.env.PORT || 3000; 
 
 const app = new Koa();
 const api = require(path.join(__dirname, 'api'));
 const etc = KoaRouter();
 
-/*
 app.use(async (ctx, next) => {
     if (ctx.request.url === '/routes')
-        console.log(api.stack.map(layer => layer.path).join('\n'));
-    next();
+        ctx.body = api.stack.map(layer => layer.path).join('\n');
+    await next();
 });
-*/
 
 const run = () => app.listen(PORT, () => {
     console.log('Up and running on port:', PORT);
 });
 
-/*
-const staticServer = serve({
-    rootDir: path.join(__dirname,'public','build'),
-    log: true
-});
-*/
-
 // use middleware
 app.use(cors());
-
-// static routes
-// app.use(staticServer);
 
 // api routes
 app.use(api.routes());
 app.use(api.allowedMethods());
+
+// static routes
+app.use(mount('/', serve('public/build')));
 
 // entry point
 module.exports = { run };
